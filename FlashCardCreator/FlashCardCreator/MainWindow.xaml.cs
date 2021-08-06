1457205c022f0ab75d9885c569cd75993abc20af
MortenJ <mortenhenningjensen@hotmail.com>
2021-08-06 10:43:07 +0200

    Ready for review

--- FlashCardCreator/FlashCardCreator/MainWindow.xaml.cs
+++ FlashCardCreator/FlashCardCreator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,30 @@ namespace FlashCardCreator
         public MainWindow()
         {
             InitializeComponent();
+            this.PreviewKeyDown += new KeyEventHandler(MainWindow_PreviewKeyDown);
+        }
+
+        private void TopOne_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var x = (Image)sender;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Uri fileUri = new Uri(openFileDialog.FileName);
+                x.Source = new BitmapImage(fileUri);
+            }
+        }
+
+        void MainWindow_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key == Key.P) && (Keyboard.Modifiers == ModifierKeys.Control))
+            {
+                PrintDialog printDialog = new PrintDialog();
+                if (printDialog.ShowDialog().GetValueOrDefault(false))
+                {
+                    printDialog.PrintVisual(this, this.Title);
+                }
+            }
         }
     }
 }
